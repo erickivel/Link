ebb8d9c12c49e49f56083b235d1a9c5f7d25f5f3
Eric Kivel <ericvkivel1@gmail.com>
2020-12-10 01:49:29 -0300

    Subscriptions

--- server/src/types/MyContext.ts
+++ server/src/types/MyContext.ts
@@ -1,7 +1,9 @@
+import { PubSub } from 'apollo-server-express';
 import { Request, Response } from 'express';
 
 export default interface MyContext {
   req: Request;
   res: Response;
   userId: string;
-};
+  pubSub: PubSub;
+}
