aa72c4b7ba8589758c59da57b74c8c9d927f1ff3
Eric Kivel <ericvkivel1@gmail.com>
2020-12-11 22:47:19 -0300

    Input component

---
+++ web/src/components/Input/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  input {
+    height: 56px;
+    width: 100%;
+    padding: 18px 24px;
+    border-radius: 10px;
+
+    border: 1px solid #342b44;
+    background: #342b44;
+    color: #ede8f4;
+
+    transition: border-color 0.2s;
+
+    &::placeholder {
+      color: #bcb2c0;
+    }
+
+    &:focus {
+      border-color: #8f57ee;
+    }
+  }
+
+  textarea {
+    resize: none;
+
+    height: 120px;
+    width: 352px;
+    padding: 18px 24px;
+    border-radius: 10px;
+
+    border: 1px solid #342b44;
+    background: #342b44;
+    color: #ede8f4;
+
+    transition: border-color 0.2s;
+
+    &::placeholder {
+      color: #bcb2c0;
+    }
+
+    &:focus {
+      border-color: #8f57ee;
+    }
+  }
+`;
